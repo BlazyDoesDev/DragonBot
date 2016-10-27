07a68e39095a8fced3b262909bfe9e546f682211
BlazyDoesDev <dragontoothybeam@outlook.com>
2016-10-27 17:35:43 +0100

    idk

--- bot.py
+++ bot.py
@@ -29,7 +29,6 @@ async def on_message(message):
                 tmp = await client.send_message(message.channel, 'Calculating messages...')
                 async for log in client.logs_from(message.channel, limit=100):
                     if log.author == message.author:
-
                         counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
