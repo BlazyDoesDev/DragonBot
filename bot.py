79b14e5e37a3e4ff3d57142320ddcaabf8a77f29
BlazyDoesDev <dragontoothybeam@outlook.com>
2016-10-29 18:51:22 +0100

    ummm

--- bot.py
+++ bot.py
@@ -29,8 +29,7 @@ async def on_message(message):
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         elif message.content.startswith('!!about'):
                 await client.send_message(message.channel, 'Hi im DragonBot')
                 await client.send_message(message.channel, 'I was devloped by @Blazy#2607')
@@ -47,9 +46,3 @@ async def on_message(message):
 
 
 client.run('MjQxMjE0Nzk3NDc4MjMyMDY0.CvO3Qg.cJO_C5TVJNYc2vsplGGQy8WhxH4') # This is a invailad token lol
-=======
-client.run('MjQxMjE0Nzk3NDc4MjMyMDY0.CvOwXg.fNFmbRgzItqn7loRylZLdUaOfds') # This is a invailad token lol
->>>>>>> parent of 6407d98... Added git add bot.pyabout
-=======
-client.run('MjQxMjE0Nzk3NDc4MjMyMDY0.CvOwXg.fNFmbRgzItqn7loRylZLdUaOfds')
->>>>>>> parent of ddc333a... hehehh kek
